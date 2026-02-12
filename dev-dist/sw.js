2d3e6ba88b0996f1c08b9f5eb454a5acb9c4fe8e
[dyad] <putanebapak03@gmail.com>
2026-02-12 18:16:18 +0700

    Update 5 file

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5119daf5'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.2aq8uh0nv9c"
+    "revision": "0.tr6ph6ktdho"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
